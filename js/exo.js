dbd9660912e8f3fb2f04d1df3d6437c438f2d88e
MathisDelrue51 <mathis.delrue@gmail.com>
2021-01-08 09:09:35 +0100

    profil hercule complété (étape  1 et 2)

--- js/exo.js
+++ js/exo.js
@@ -1,1 +1,13 @@
 console.log('test 1...2...1...2');
+
+const hercule ={
+    name :'Hercule',
+    job: 'Demi-dieu',
+    age: 35,
+    departement: 75,
+    arm: 60.5,
+    inRelationship: true,
+
+}
+
+base.fillProfil(hercule);
