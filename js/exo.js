f81b43729f7e6ed885c65ace11615f52c53bd972
MathisDelrue51 <mathis.delrue@gmail.com>
2021-01-08 11:54:03 +0100

    finalement j'arrive a afficher des pourcentages de popularité pour hercule et JC

--- js/exo.js
+++ js/exo.js
@@ -1,5 +1,5 @@
-const page ={
-    hercule : {
+const page = {
+    hercule: {
         name: 'Hercule',
         job: 'Demi-dieu',
         age: 35,
@@ -8,9 +8,9 @@ const page ={
         inRelationship: true,
     },
 
-    friends : ['Jupiter', 'Junon', 'Alcmène', 'Déjanire'],
+    friends: ['Jupiter', 'Junon', 'Alcmène', 'Déjanire'],
 
-    addTitle: function() {
+    addTitle: function () {
         const title = document.createElement('h1');
         title.className = 'banner_title';
         title.textContent = 'Vous consultez le profil de Hercule';
@@ -18,25 +18,25 @@ const page ={
         header.appendChild(title);
     },
 
-    addWork: function() {
+    addWork: function () {
 
         for (let index = 0; index <= 11; index++) {
             base.displayWork(index);
         }
     },
-    
-    generatePseudo: function(name, departement) {
+
+    generatePseudo: function (name, departement) {
         return name + "-du-" + departement;
     },
 
-     writePseudo: function() {
+    writePseudo: function () {
         const profilContent = page.generatePseudo(page.hercule.name, page.hercule.departement);
         console.log(profilContent);
         const profil = document.getElementById('profil-name');
         profil.textContent = profilContent;
     },
-    
-    openCloseMenu: function(event) {
+
+    openCloseMenu: function (event) {
         console.log("openCloseMenu");
         const banner = document.getElementById('header-banner');
         if (banner.classList.contains('banner--open')) {
@@ -45,30 +45,42 @@ const page ={
             banner.classList.add('banner--open');
         }
     },
-    
-    contactHercule: function(event) {
+
+    contactHercule: function (event) {
         event.preventDefault();
-        alert("Hercule ne souhaite pas être dérangé");    
+        alert("Hercule ne souhaite pas être dérangé");
     },
 
-    init: function() {
+    displayVote: function () {
+        const totalVotes = base.vote.hercule + base.vote.cesar;
+
+        const herculePopularity = document.querySelector('#trends-hercule .people__popularity');
+        const cesarPopularity = document.querySelector('#trends-cesar .people__popularity');  
+
+        herculePopularity.textContent = Math.round(100 * base.vote.hercule / totalVotes) + "%";
+        cesarPopularity.textContent = Math.round(100 * base.vote.cesar / totalVotes) + "%";
+    },
+
+    init: function () {
         base.fillProfil(page.hercule);
-    
+
         base.printFriends(page.friends);
-    
+
         base.setBestFriend(page.friends[0]);
-    
+
         page.addTitle();
-    
+
         page.addWork();
-    
+
         page.writePseudo();
-    
+
         const menu = document.getElementById('menu-toggler');
         menu.addEventListener('click', page.openCloseMenu);
-    
+
         const form = document.getElementById('contact');
         form.addEventListener('submit', page.contactHercule);
+
+        page.displayVote();
     }
 
 }
